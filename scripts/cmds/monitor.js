f2fb1e54f6be3aaf702e93af9efd96650f73b2cf
Ishmababe <ishababe587@gmail.com>
2024-10-17 10:41:56 +0545

    Update monitor.js

--- scripts/cmds/monitor.js
+++ scripts/cmds/monitor.js
@@ -23,7 +23,7 @@ module.exports = {
       const r = Math.floor(Math.random() * s.length);
       const q = s[r];
 
-      const u = `https://pin-two.vercel.app/pin?search=${encodeURIComponent(q)}`;
+      const u = `https://pin-kshitiz.vercel.app/pin?search=${encodeURIComponent(q)}`;
 
       const a = await axios.get(u);
       const l = a.data.result;

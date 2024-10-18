e0083f94e764b6a18f920e493dc609f16bae402e
Ishmababe <ishababe587@gmail.com>
2024-10-18 19:40:10 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,7 +44,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😾"){
-  if(event.userID == "100088286122703" ){
+  if(event.userID == "100088286122703" ,"61555139502161" ){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -54,7 +54,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🤍"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100088286122703"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100088286122703","61555139502161"){
     message.unsend(event.messageID)
 }else{
     message.send("")

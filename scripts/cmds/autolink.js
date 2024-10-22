4944c0241314adef6a433faa54e24272adc91c28
Ishmababe <ishababe587@gmail.com>
2024-10-22 23:56:17 +0545

    Update autolink.js

--- scripts/cmds/autolink.js
+++ scripts/cmds/autolink.js
@@ -80,7 +80,7 @@ module.exports = {
       } else {
         api.sendMessage("", event.threadID, event.messageID);
       }
-      api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+      api.setMessageReaction("💯", event.messageID, (err) => {}, true);
     }
   },
   downLoad: function (url, api, event) {
@@ -115,7 +115,7 @@ module.exports = {
       }
 
       const shortUrl = await shortenURL(res);
-      const messageBody = `✅ 🔗 Download Url: ${shortUrl}`;
+      const messageBody = ``;
       api.sendMessage({
         body: messageBody,
         attachment: fs.createReadStream(path)
@@ -140,7 +140,7 @@ module.exports = {
         response.data.pipe(fs.createWriteStream(path));
         response.data.on('end', async () => {
           const shortUrl = await shortenURL(videoUrl);
-          const messageBody = `✅🔗 Download Url: ${shortUrl}`;
+          const messageBody = ``;
 
           api.sendMessage({
             body: messageBody,
@@ -170,7 +170,7 @@ module.exports = {
         response.data.pipe(fs.createWriteStream(path));
         response.data.on('end', async () => {
           const shortUrl = await shortenURL(videoUrl);
-          const messageBody = `✅🔗 Download Url: ${shortUrl}`;
+          const messageBody = ``;
 
           api.sendMessage({
             body: messageBody,
@@ -202,7 +202,7 @@ module.exports = {
       response.data.pipe(fs.createWriteStream(path));
       response.data.on('end', async () => {
         const shortUrl = await shortenURL(videoUrl);
-        const messageBody = `✅🔗 Download Url: ${shortUrl}`;
+        const messageBody = ``;
 
         api.sendMessage({
           body: messageBody,
@@ -231,7 +231,7 @@ module.exports = {
       response.data.pipe(fs.createWriteStream(path));
       response.data.on('end', async () => {
         const shortUrl = await shortenURL(videoUrl);
-        const messageBody = `✅🔗 Download Url: ${shortUrl}`;
+        const messageBody = ``;
 
         api.sendMessage({
           body: messageBody,
@@ -260,7 +260,7 @@ module.exports = {
       response.data.pipe(fs.createWriteStream(path));
       response.data.on('end', async () => {
         const shortUrl = await shortenURL(videoUrl);
-        const messageBody = `✅🔗 Download Url: ${shortUrl}`;
+        const messageBody = ``;
 
         api.sendMessage({
           body: messageBody,
@@ -448,4 +448,4 @@ async function fbDownloader(url) {
       success: false
     };
   }
-        }
+          }
